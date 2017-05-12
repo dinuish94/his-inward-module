a32d9a734202713815f0b229aabe531b0eccc035
Jonathan Deon <Jonathan Deon>
2017-05-12 12:31:23 +0530

    re-edited server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app =  express();
 
 app.use(bodyParser.json());
 
-mongoose.connect('localhost:27017/bht', err => {
+mongoose.connect('mongodb://localhost:27017/wards', err => {
     if (err) {
         console.log(err);
         process.exit(1);
@@ -18,9 +18,15 @@ app.use('/app/modules',express.static(__dirname + "/bower_components"));
 
 mongoose.Promise = global.Promise;
 
+
+require('./server/ward/ward.model.js');
+require('./server/bed/bed.model.js');
+const WardRouter = require('./server/ward/ward.route.js');
+
 require('./server/BHT/labTest.model.js');
 require('./server/BHT/labTestTypes.model.js');
 const LabRouter = require('./server/BHT/labTest.route.js');
+
 
 
 app.get('/', function(req,res){
@@ -28,6 +34,7 @@ app.get('/', function(req,res){
 });
 
 app.use('/labTests', LabRouter);
+app.use('/wards', WardRouter);
 
 app.get('/app/*', function(req,res){
     res.sendFile(__dirname + '/public/index.html');
